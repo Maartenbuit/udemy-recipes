38c1ed785bcb4337168b792e1e85a8a37d91f81f
Maartenbuit <maartenbuitelaar@hotmail.com>
2019-12-11 09:39:14 +0100

    add recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('a Test recipe',
+      'this is a test',
+      'https://get.pxhere.com/photo/restaurant-dish-meal-food-produce-kitchen-recipe-healthy-snack-meat-lunch-cuisine-health-hamburger-sandwich-exotic-slider-veggie-burger-1053018.jpg'
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
 
+  }
 }
